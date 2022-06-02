4684851d68b64c8d3d28eecb9625707dd4af496b
Jeff Hoang <jeff.hoang@crypto.com>
2022-06-02 12:55:00 +0700

    Add Koin for launching services

--- feature-a/impl/src/main/java/com/example/feature_a/impl/feature/ActivityA.kt
+++ feature-a/impl/src/main/java/com/example/feature_a/impl/feature/ActivityA.kt
@@ -2,11 +2,29 @@ package com.example.feature_a.impl.feature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.example.feature_a.impl.R
+import com.example.feature_b.api.repo.IRepositoryB
+import com.example.feature_b.api.service.IServiceB
+import org.koin.android.ext.android.inject
 
 class ActivityA : AppCompatActivity() {
+    private val serviceB: IServiceB by inject()
+    private val repoB: IRepositoryB by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a)
+
+        findViewById<Button>(R.id.btn_launch_b).setOnClickListener {
+            serviceB.launchB(this)
+        }
+
+        findViewById<Button>(R.id.btn_fetch_b).setOnClickListener {
+            repoB.introB().let {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- feature-b/impl/src/main/java/com/example/feature_b/impl/feature/ActivityB.kt
+++ feature-b/impl/src/main/java/com/example/feature_b/impl/feature/ActivityB.kt
@@ -1,12 +1,30 @@
 package com.example.feature_b.impl.feature
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.feature_a.api.IRepositoryA
+import com.example.feature_a.api.IServiceA
 import com.example.feature_b.impl.R
+import org.koin.android.ext.android.inject
 
 class ActivityB: AppCompatActivity() {
+    private val serviceA: IServiceA by inject()
+    private val repoA: IRepositoryA by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b)
+
+        findViewById<Button>(R.id.btn_launch_a).setOnClickListener {
+            serviceA.launchA(this)
+        }
+
+        findViewById<Button>(R.id.btn_fetch_a).setOnClickListener {
+            repoA.introA().let {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
